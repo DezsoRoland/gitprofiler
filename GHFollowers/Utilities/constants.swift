3621070e2f9625695c009c36ead210ca090e5c6f
DezsoRoland <dezsoroland92@gmail.com>
2020-10-13 18:00:59 +0200

    added child view controller - User info header viewcontroller

---
+++ GHFollowers/Utilities/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constans.swift
+//  GHFollowers
+//
+//  Created by Roland on 10/10/2020.
+//  Copyright © 2020 Roland. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
